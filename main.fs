b18a6f4e8fff66d2aaa095c22bf665a06b97b72d
saul <saul.field@gmail.com>
2019-10-20 02:05:04 -0400

    Basic parsing

--- main.fs
+++ main.fs
@@ -2,11 +2,6 @@
 
 // Types
 
-type Keyword =
-  | Return
-  | Void
-  | Int
-
 type Token =
   | OpenBrace
   | CloseBrace
@@ -15,7 +10,9 @@ type Token =
   | Semicolon
   | Identifier of string
   | Int of int
-  | Keyword of Keyword
+  | KeywordReturn
+  | KeywordVoid
+  | KeywordInt
   | EOF
   | Unknown
 
@@ -39,9 +36,9 @@ let lexInt chars =
 
 let getIdentOrKeyword str =
   match str with
-  | "int"    -> Keyword Keyword.Int
-  | "void"   -> Keyword Void
-  | "return" -> Keyword Return
+  | "int"    -> KeywordInt
+  | "void"   -> KeywordVoid
+  | "return" -> KeywordReturn
   | _ -> Identifier str
 
 let lexIdent chars =
@@ -58,8 +55,8 @@ let lexOther chars =
   match chars with
   | [] -> EOF,[]
   | c::rest when Char.IsLetter c || c = '_' -> lexIdent chars
-  | c::rest when Char.IsDigit  c            -> lexInt chars
-  | _ -> Unknown, chars
+  | c::rest when Char.IsDigit c             -> lexInt chars
+  | c::rest -> failwithf "Lex failed with char: %A" c
 
 let rec lex chars =
   match chars with
@@ -75,6 +72,78 @@ let rec lex chars =
         let token,rest = lexOther chars
         token::(lex rest)
 
+// Parse
+
+// <program> ::= <function>
+// <function> ::= "int" <id> "(" "void" ")" "{" <statement> "}"
+// <statement> ::= "return" <exp> ";"
+// <exp> ::= <int>
+
+type Exp =
+  | Int of int
+
+type Stmt =
+  | Return of Exp
+
+type FuncDecl = {
+  Ident:string;
+  Stmt:Stmt
+}
+
+let parseError msg =
+  failwithf "Parse error: %s" msg
+
+let parseExp tokens =
+  match tokens with
+  | (Token.Int intVal)::rest -> Exp.Int intVal, rest
+  | _ -> parseError "expected int literal"
+
+let parseStmt tokens =
+  let exp,tokens = 
+    match tokens with
+    | KeywordReturn::rest -> parseExp rest
+    | _ -> parseError "invalid statement"
+
+  let tokens = 
+    match tokens with
+    | Semicolon::rest -> rest
+    | _ -> parseError "expected semicolon"
+
+  Stmt.Return exp,tokens
+
+let parseFuncDecl tokens =
+  let tokens =
+    match tokens with
+    | KeywordInt::rest -> rest
+    | _ -> parseError "unexpected type"
+
+  let ident,tokens =
+    match tokens with
+    | (Identifier ident)::rest -> ident,rest
+    | _ -> parseError "expected Identifier"
+
+  let tokens =
+    match tokens with
+    | OpenParen::KeywordVoid::CloseParen::rest -> rest
+    | _ -> parseError "invalid arg list"
+
+  let stmt,tokens =
+    match tokens with
+    | OpenBrace::rest -> parseStmt rest
+    | _ -> parseError "expected open brace"
+
+  let tokens =
+    match tokens with
+    | CloseBrace::rest -> rest
+    | _ -> parseError "expected close brace"
+
+  { Ident=ident; Stmt=stmt },tokens
+
+let parse toks =
+  match toks with
+  | KeywordInt::rest -> parseFuncDecl toks
+  | _ -> parseError "expected 'int' keyword"
+
 // Main
 
 [<EntryPoint>]
@@ -84,7 +153,10 @@ let main argv =
   let chars = Seq.toList text
   let tokens = lex chars
 
-  for token in tokens do
-    printfn "%A" token
+  // for token in tokens do
+  //   printfn "%A" token
+
+  let ast,_ = parse tokens
+  printfn "%A" ast
 
   0
