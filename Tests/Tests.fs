e10086b91cda2f658ec8cc5d50e93d8316a1018a
Saul <saul.field@gmail.com>
2020-03-08 00:05:53 -0500

    Added parser tests

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -7,10 +7,7 @@ open System
 
 #nowarn "3242"
 
-type Tests () =
-
-  let log (msg : obj) = 
-    NUnit.Framework.TestContext.WriteLine msg
+type LexerTests () =
 
   [<SetUp>]
   member this.Setup () =
@@ -24,15 +21,51 @@ type Tests () =
 
     let tokens = lex chars
     for token in tokens do printfn "%A" token
+       
+type ParserTests () =
 
-  [<Test>]
-  member this.TestParser () =
-    let testFilename = __SOURCE_DIRECTORY__ + "/../Examples/parse-test.c"
-    let text = IO.File.ReadAllText testFilename
+  let parseTest text =
     let chars = Seq.toList text
-
     let tokens = lex chars
     for token in tokens do printfn "%A" token
-
     let ast = parse tokens
     printfn "%A" ast
+
+  [<SetUp>]
+  member this.Setup () =
+    ()
+
+  [<Test>]
+  member this.ParseExampleFile () =
+    let testFilename = __SOURCE_DIRECTORY__ + "/../Examples/parse-test.c"
+    let text = IO.File.ReadAllText testFilename
+    parseTest text
+
+  [<Test>]
+  member this.ParseIfElseBlocks () =
+    let text = "
+    int main(void) {
+      int x = 0;
+      if (x > 0) {
+        x = 0;
+        x = x + 1;
+      } else {
+        x = 0;
+        x = x - 1;
+      }
+      return 0;
+    }"
+    parseTest text
+
+  [<Test>]
+  member this.ParseIfElseStatements () =
+    let text = "
+    int main(void) {
+      int x = 0;
+      if (x > 0)
+        x = x + 1;
+      else
+        x = x - 1;
+      return 0;
+    }"
+    parseTest text
